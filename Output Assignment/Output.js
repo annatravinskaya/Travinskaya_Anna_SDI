1ff0faeaf693a971ade46173d341e3465174befd
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-05 06:32:06 +0200

    Added a header title.
    Added a header title.

--- Output Assignment/Output.js
+++
@@ -1,3 +0,0 @@
-//Anna Travinskaya
-// Scalable Data Infrastructures 201510-01 Output Assignment //
-
---
+++ Travinskaya_Anna_Output_Assignment/Output.js
@@ -0,0 +1,3 @@
+//Anna Travinskaya
+// Scalable Data Infrastructures 201510-01 Travinskaya_Anna_Output_Assignment //
+
