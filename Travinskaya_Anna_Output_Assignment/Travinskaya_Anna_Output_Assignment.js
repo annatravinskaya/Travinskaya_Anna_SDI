56f84074f5528b5898ba3af247e68792f0626db1
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-05 20:34:21 +0200

    Added comment to the second line
    Added comment to the second line.

--- Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
+++ Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
@@ -2,5 +2,5 @@
 // Scalable Data Infrastructures 201510-01 Travinskaya_Anna_Output_Assignment //
 
 var myPlaceOfLiving = "Switzerland"; // declared and defined my first variable
-var numberOfYear = 6;
+var numberOfYear = 6; // declared and defined my number variable
 var swissNationality = false;
