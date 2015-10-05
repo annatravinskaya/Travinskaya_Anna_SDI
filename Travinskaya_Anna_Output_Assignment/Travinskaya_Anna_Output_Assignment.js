a954201117e1ad76a874553aa1c73a4987144b52
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-05 21:03:10 +0200

    Created a text with my number variable
    Created a text with my number variable.

--- Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
+++ Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
@@ -5,6 +5,9 @@ var myPlaceOfLiving = "Switzerland"; // declared and defined my string variable
 var numberOfYears = 6; // declared and defined my number variable
 var swissNationality = false; // declared and defined my Boolean variable
 
-document.write("Hello, I live in " + myPlaceOfLiving + " for " + numberOfYears + " years ");
+
+document.write(" Hello , I live in " + myPlaceOfLiving + " for " + numberOfYears + " years. ");
+
+document.write(numberOfYears + " years is a lot. ");
 
 
