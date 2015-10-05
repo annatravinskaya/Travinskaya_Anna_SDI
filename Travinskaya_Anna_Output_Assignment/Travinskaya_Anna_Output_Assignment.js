5ff8f5dd3a1bd5c28497999b6e66acd957f1eb22
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-05 20:29:32 +0200

    Declared and defined my second variable
    Declared and defined my second variable

--- Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
+++ Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
@@ -1,4 +1,5 @@
 //Anna Travinskaya
 // Scalable Data Infrastructures 201510-01 Travinskaya_Anna_Output_Assignment //
 
-var myPlaceOfLiving = "Switzerland" // declared and defined my first variable
+var myPlaceOfLiving = "Switzerland"; // declared and defined my first variable
+var numberOfYear = 6;
