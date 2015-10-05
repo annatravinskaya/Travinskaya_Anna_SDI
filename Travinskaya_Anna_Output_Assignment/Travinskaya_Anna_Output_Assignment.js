bd300d4466e1fdf1ffa9ac7605e46a1122142c61
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-05 20:37:37 +0200

    Changed a comment for the first line.
    Replaced the word “first” with the “string”.

--- Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
+++ Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
@@ -1,6 +1,6 @@
 //Anna Travinskaya
 // Scalable Data Infrastructures 201510-01 Travinskaya_Anna_Output_Assignment //
 
-var myPlaceOfLiving = "Switzerland"; // declared and defined my first variable
-var numberOfYear = 6; // declared and defined my number variable
+var myPlaceOfLiving = "Switzerland"; // declared and defined my string variable
+var numberOfYears = 6; // declared and defined my number variable
 var swissNationality = false; // declared and defined my Boolean variable
