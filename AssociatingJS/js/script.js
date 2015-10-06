2bb1641b72877f20561715a00e25f83b563e36f8
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-06 20:50:11 +0200

    Expressions: Practice of assignment operators
    Increment. Decrement.  Special shortcuts.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,9 @@
-//Expressions - Concatenating Strings
+//Expressions - Assignment Operators
 
-var a = "6";
-var b = '7';
+var a = 3;
+//a ++ ; // a = a + 1 // a += 1
+a --; // a = a - 1 // a -= 1
+console.log(a);
 
-var result = a + b;
 
-console.log(result);
+
