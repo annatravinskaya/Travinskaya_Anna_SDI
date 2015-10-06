8a2ae284535da3e886160445358e94d914bb9f31
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-06 18:09:50 +0200

    Expressions: Concatenating Strings
    Number example

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,8 @@
 //Expressions - Concatenating Strings
 
-var firstName = "Anna";
-var lastName = 'Travinskaya';
-var fullName = firstName + " " + lastName;
+var a = "6";
+var b = '7';
 
-console.log(fullName);
+var result = a + b;
 
-
+console.log(result);
