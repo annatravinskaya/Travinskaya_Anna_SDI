6b7b89eafa407440cf326816c6c509a856770351
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-06 20:57:47 +0200

    Casting: Adding number in front of a string.
    Casting: Adding number in front of a string.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,9 @@
-//Expressions - Assignment Operators
+//Casting
 
-var a = 3;
-//a ++ ; // a = a + 1 // a += 1
-a --; // a = a - 1 // a -= 1
-console.log(a);
+var stringVar = '6';
+var result = 7 + Number(stringVar);
+console.log(result);
+
 
 
 
