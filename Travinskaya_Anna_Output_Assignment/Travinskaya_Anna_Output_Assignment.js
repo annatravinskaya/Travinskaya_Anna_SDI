919dfd49c55e376c071122171915a59d2862c034
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-07 16:32:42 +0200

    Commit changes to the output.
    Changed document.write to console.log.
    Also formatted the comments.

--- Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
+++ Travinskaya_Anna_Output_Assignment/Travinskaya_Anna_Output_Assignment.js
@@ -1,15 +1,16 @@
-//Anna Travinskaya
-// Scalable Data Infrastructures 201510-01 Travinskaya_Anna_Output_Assignment //
-// 5.10.2015//
+/** Anna Travinskaya
+* Scalable Data Infrastructures 201510-01 Output Assignment
+* 5.10.2015
+ * */
 
-var myPlaceOfLiving = "Switzerland"; // declared and defined my string variable //
-var numberOfYears = 6; // declared and defined my number variable //
-var swissNationality = false; // declared and defined my Boolean variable //
+var myPlaceOfLiving = "Switzerland"; // declared and defined my string variable
+var numberOfYears = 6; // declared and defined my number variable
+var swissNationality = false; // declared and defined my Boolean variable
 
 
-document.write(" Hello, I live in " + myPlaceOfLiving + " for " + numberOfYears + " years. "); // Concatenated output for my string and number variables.//
+console.log(" Hello, I live in " + myPlaceOfLiving + " for " + numberOfYears + " years. "); // Concatenated output for my string and number variables.
 
-document.write( numberOfYears + " years is a lot. "); // Concatenated output for my number variable. //
+console.log( numberOfYears + " years is a lot. "); // Concatenated output for my number variable.
 
-document.write("However, it is " + swissNationality + " to assume that I have a Swiss nationality."); // Concatenated output for my Boolean variable. //
+console.log("However, it is " + swissNationality + " to assume that I have a Swiss nationality."); // Concatenated output for my Boolean variable.
 
