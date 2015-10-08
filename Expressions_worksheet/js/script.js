04390afb04beaa067bfeaf10bd83c810906a22aa
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 18:27:44 +0200

    Added comments and declared the variables.
    Added comments and declared  variables.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,2 +1,15 @@
-// Anna Travinskaya 7.10.15 Expressions Worksheet
+//Anna Travinskaya 7.10.15 Expressions Worksheet
+
+//Calculate the dog age of Sparky
+
+//Variables
+var humanAgeOfSparky = 3;
+//1 year old dog = 7 dog years. Therefore, Sparky's age in human years is multiplied by 7 to understand his dog age.
+var dogAgeOfSparky = humanAgeOfSparky * 7; //
+
+
+
+
+
+
 
