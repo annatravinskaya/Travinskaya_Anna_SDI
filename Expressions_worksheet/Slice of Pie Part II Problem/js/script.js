2dc2d973b18bc0ef7c70c43f2f2da02596419db3
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 22:42:59 +0200

    Declared variables and added comments.
    Declared variables and added comments.

---
+++ Expressions_worksheet/Slice of Pie Part II Problem/js/script.js
@@ -0,0 +1,13 @@
+/** Anna Travinskaya 8.10.15 Expressions Worksheet */
+
+// Problem: Slice of Pie Part II
+
+//Calculate how many leftover pizzas the dog will get
+//Variables
+var slicesPerPizza = 7;
+var numberOfPeople = 16;
+var pizzasOrdered = 5;
+
+var slicesForSparky = slicesPerPizza * pizzasOrdered % numberOfPeople; //result variable
+
+
