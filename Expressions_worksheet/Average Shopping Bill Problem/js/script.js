b82081e9ba3abea87d4ead7ed929f5e648225c98
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 23:05:04 +0200

    Added variables and comments.
    Added variables and comments.

---
+++ Expressions_worksheet/Average Shopping Bill Problem/js/script.js
@@ -0,0 +1,13 @@
+/** Anna Travinskaya 8.10.15 Expressions Worksheet */
+
+//Average Shopping Bill Problem
+
+// Array Variable
+var weeklyGroceryTotal = [91,101,105,98,101];
+
+
+//Sum of all numbers = total amount of money spent on groceries
+var totalAmount = weeklyGroceryTotal[0] + weeklyGroceryTotal[1] + weeklyGroceryTotal[2] + weeklyGroceryTotal[3] + weeklyGroceryTotal[4];
+
+//Average = Sum of all numbers divided by the amount of numbers there are.
+var averageWeeklySpending = totalAmount / 5;
