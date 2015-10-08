bf2e74cac94e6138ce897bcb04164520427a5b73
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 18:41:59 +0200

    Created an output
    Created an output for the problem.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,6 +7,8 @@ var humanAgeOfSparky = 3;
 //1 year old dog = 7 dog years. Therefore, Sparky's age in human years is multiplied by 7 to understand his dog age.
 var dogAgeOfSparky = humanAgeOfSparky * 7; //
 
+//print out the output string that explains how many human years Sparky has and how this translates into dog years.
+console.log('Sparky is ' + humanAgeOfSparky + ' human years old which is ' + dogAgeOfSparky + ' in dog years.')
 
 
 
