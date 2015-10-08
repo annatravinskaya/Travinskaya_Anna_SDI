67c08f08caaa8962a2d1b90d44d099ec67fadfcc
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 22:21:40 +0200

    Commit changes to code.
    Added the result variable and the output. Comments added as well.

--- Expressions_worksheet/Slice of Pie Part I Problem/js/script.js
+++ Expressions_worksheet/Slice of Pie Part I Problem/js/script.js
@@ -4,13 +4,15 @@
 //Problem: Slice of Pie Part I
 
 //calculate how many slices of pizza each person gets
-
+//Variables
 var slicesPerPizza = 7;
 var numberOfPeople = 16;
 var pizzasOrdered = 5;
 
-var slicesPerPerson = slicesPerPizza * pizzasOrdered / numberOfPeople;
+var slicesPerPerson = slicesPerPizza * pizzasOrdered / numberOfPeople; // result variable
 
+//Print out the output describing the result
+console.log('Each person ate ' + slicesPerPerson + " slices of pizza at the party.");
 
 
 
