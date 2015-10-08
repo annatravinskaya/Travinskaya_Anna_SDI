8ad3589b4e887dcfded928a2ad59721ccae15043
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 22:01:23 +0200

    Declared variables and made comments
    Declared variables and made comments

--- Expressions_worksheet/Slice of Pie Part I Problem/js/script.js
+++ Expressions_worksheet/Slice of Pie Part I Problem/js/script.js
@@ -1,2 +1,13 @@
 /** Anna Travinskaya 8.10.15 Expressions Worksheet*/
 
+
+//Problem: Slice of Pie Part I
+
+//calculate how many slices of pizza each person gets
+
+var slicesPerPizza = 7;
+var numberOfPeople = 27;
+var pizzasOrdered = 5;
+
+
+
