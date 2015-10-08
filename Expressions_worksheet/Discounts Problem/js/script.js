167fbaabc6ab61ed15d3a001d36dbbb383c4833b
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 23:33:14 +0200

    Added result variables and comments.
    Added result variables and comments.

--- Expressions_worksheet/Discounts Problem/js/script.js
+++ Expressions_worksheet/Discounts Problem/js/script.js
@@ -3,9 +3,14 @@
 //Discounts Problem
 
 //Variables
-var originalPrice = Number"$80";
-var discountPercentage = Number'15 percent';
+var originalPrice = 80;
+var discountPercentage = 15;
 var itemDescription = "pullover";
-var salesTaxPercentage = Number"1.5 percent";
+var salesTaxPercentage = 1.5;
 
+// Original price times the  discount percentage and divided by 100 = discount amount
+//Original price - discount amount = price without tax
+// Price with tax = price without tax plus the tax percentage
+var priceWithoutTax = originalPrice - (originalPrice * discountPercentage / 100);
+var priceWithTax =  priceWithoutTax + (priceWithoutTax * salesTaxPercentage / 100);
 
