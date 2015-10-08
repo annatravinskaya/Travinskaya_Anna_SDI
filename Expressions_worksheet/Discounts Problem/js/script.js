5d8b1ed3b6b096d24901b1366fd048319de9eb2f
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-08 23:19:50 +0200

    Declared variables and added comments.
    Declared variables and added comments

---
+++ Expressions_worksheet/Discounts Problem/js/script.js
@@ -0,0 +1,11 @@
+/** Anna Travinskaya 8.10.15 Expressions Worksheet */
+
+//Discounts Problem
+
+//Variables
+var originalPrice = Number"$80";
+var discountPercentage = Number'15 percent';
+var itemDescription = "pullover";
+var salesTaxPercentage = Number"1.5 percent";
+
+
