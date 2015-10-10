630436f24ad990d55ac5aba48073532971c79210
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-10 18:52:30 +0200

    Added a Boolean variable.
    Added a Boolean variable.

--- Travinskaya_Anna_Expressions_Assignment/js/script.js
+++ Travinskaya_Anna_Expressions_Assignment/js/script.js
@@ -3,19 +3,21 @@
 
 //Variables
 var userAge = prompt("Enter your name: ");
-var userHeight = prompt("Enter your height in cm: ");
+var ageLessThan20 = false; // if the user is less than 20 years old than the results would be described differently than using this calculator.
 var userWeight = prompt("Enter your weight in kg: ");
+var userHeight = prompt("Enter your height in cm: ");
 
-var userHeight = Number;//casted the variable as a number
+//Casted variables as numbers
+userAge = Number;
+userWeight = Number;
+userHeight = Number;
 
+//Steps to calculate BMI
 // to get height in meters: height in cm needs to be divided by 100 and multiplied by itself.
 var metricHeight = userHeight / 100 * userHeight; // used arithmetic operators
 
 //to calculate Body Mass Index: weight needs to be divided by by metric height.
 var userBMI = Number(userWeight) / metricHeight;//
-
-
-
 
 
 
