3d80c438f781381ec17942839bd480c019c44c90
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-10 18:06:02 +0200

    Specified further the userHeight variable.
    Specified further the userHeight variable. Used some arithmetic
    assignment operators to define it. Made comments.

--- Travinskaya_Anna_Expressions_Assignment/js/script.js
+++ Travinskaya_Anna_Expressions_Assignment/js/script.js
@@ -3,7 +3,14 @@
 
 //Variables
 var userAge = prompt("Enter your name: ");
-var userHeight = prompt("Enter your height in cm: );
-var userWeight = prompt("Enter your weight in kg: )";
+var userHeight = prompt("Enter your height in cm: ");
+var userWeight = prompt("Enter your weight in kg: ");
+
+var userHeight = Number;//casted the variable as a number
+var userHeight /= 100; // used arithmetic assignment operator
+var userHeight *= userHeight; // used second arithmetic assignment operator
+
+
+
 
 
