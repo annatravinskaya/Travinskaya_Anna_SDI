d6a82fedbe048e3ab2709bae3b43532c64531c17
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-10 23:21:24 +0200

    Changed the definitions and added the output.
    Changed the definitions and added the output.

--- Travinskaya_Anna_Expressions_Assignment/js/script.js
+++ Travinskaya_Anna_Expressions_Assignment/js/script.js
@@ -2,21 +2,25 @@
  */
 
 //Variables
-var userAge = prompt("Enter your name: ");
+var userAge = prompt("Enter your age: ");
 var userWeight = prompt("Enter your weight in kg: ");
 var userHeight = prompt("Enter your height in cm: ");
 
 //Casted variables as numbers
-userAge = Number > 20; // if the user is less than 20 than he or she is too young to receive an accurate discription using this calculator
-userWeight = Number;
-userHeight = Number;
+userAge = Number(userAge) > 20; // if the user is less than 20 than he or she is too young to receive an accurate discription using this calculator
+userWeight = Number(userWeight);
+userHeight = Number(userHeight);
 
 //Steps to calculate BMI
 // to get height in meters: height in cm needs to be divided by 100 and multiplied by itself.
-var metricHeight = userHeight / 100 * userHeight; // used arithmetic operators
+var metricHeight = (userHeight / 100) * (userHeight / 100) ; // used arithmetic operators
 
 //to calculate Body Mass Index: weight needs to be divided by by metric height.
-var userBMI = Number(userWeight) / metricHeight;//
+var userBMI = (userWeight) / (metricHeight);//
+
+console.log(userBMI);
+
+
 
 
 
