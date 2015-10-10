5bc5efb34d4d2fcba16bcfff86abfac4249f200f
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-10 18:26:23 +0200

    Added a result variable and formula to find the answer.
    Added a result variable and formula to find the answer.

--- Travinskaya_Anna_Expressions_Assignment/js/script.js
+++ Travinskaya_Anna_Expressions_Assignment/js/script.js
@@ -7,7 +7,13 @@ var userHeight = prompt("Enter your height in cm: ");
 var userWeight = prompt("Enter your weight in kg: ");
 
 var userHeight = Number;//casted the variable as a number
+
+// to get height in meters: height in cm needs to be divided by 100 and multiplied by itself.
 var metricHeight = userHeight / 100 * userHeight; // used arithmetic operators
+
+//to calculate Body Mass Index: weight needs to be divided by by metric height.
+var userBMI = Number(userWeight) / metricHeight;//
+
 
 
 
