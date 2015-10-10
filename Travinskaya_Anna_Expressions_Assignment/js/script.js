b6bf7cb116bbaeaca988b140101c804ff92cc4b8
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-10 19:00:39 +0200

    Got rid of Boolean value and improved definition of the age variable.
    Got rid of Boolean value and improved definition of the age variable.

--- Travinskaya_Anna_Expressions_Assignment/js/script.js
+++ Travinskaya_Anna_Expressions_Assignment/js/script.js
@@ -3,12 +3,11 @@
 
 //Variables
 var userAge = prompt("Enter your name: ");
-var ageLessThan20 = false; // if the user is less than 20 years old than the results would be described differently than using this calculator.
 var userWeight = prompt("Enter your weight in kg: ");
 var userHeight = prompt("Enter your height in cm: ");
 
 //Casted variables as numbers
-userAge = Number;
+userAge = Number > 20; // if the user is less than 20 than he or she is too young to receive an accurate discription using this calculator
 userWeight = Number;
 userHeight = Number;
 
@@ -18,6 +17,9 @@ var metricHeight = userHeight / 100 * userHeight; // used arithmetic operators
 
 //to calculate Body Mass Index: weight needs to be divided by by metric height.
 var userBMI = Number(userWeight) / metricHeight;//
+
+
+
 
 
 
