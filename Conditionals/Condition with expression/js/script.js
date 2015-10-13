5fa0229d6cc331ec513bd6d2044076c983159aa8
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-13 17:08:35 +0200

    Conditionals practice with expressions.
    Conditionals practice with expressions. Code didn’t work.

---
+++ Conditionals/Condition with expression/js/script.js
@@ -0,0 +1,9 @@
+//Conditional Logic - with an expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+if(kidHeight + sneakerLift > minHeight){
+    console.log("You can ride the coaster!");
+}
