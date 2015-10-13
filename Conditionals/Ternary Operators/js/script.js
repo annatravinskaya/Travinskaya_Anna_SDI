a56e982d7a18affd5ebda96ce50bbdb3161b5e0c
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-13 21:07:10 +0200

    Book examples .
    Ternary operators: book example.

--- Conditionals/Ternary Operators/js/script.js
+++ Conditionals/Ternary Operators/js/script.js
@@ -1,6 +1,6 @@
 //Conditional Logic - Ternary Operators
 
-var gpa = 48;
+//var gpa = 48;
 
 //if the gpa is over the min 2.0 score, the student can graduate
 
@@ -10,4 +10,12 @@ var gpa = 48;
     console.log("GPA is too low!");
 }*/
 
-(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+//(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+
+var age = 11;
+var book;
+
+//If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
+
