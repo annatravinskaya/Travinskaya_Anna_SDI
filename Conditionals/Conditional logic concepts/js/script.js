7497422c80a93df2978ad0c7db208c8efaf59699
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-13 14:21:10 +0200

    Logic concepts practice
    Used comparative operators.

---
+++ Conditionals/Conditional logic concepts/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by annatravinskaya on 13/10/15.
+ */
+
+var temp;
+var goToTheBeach;
+
+if (temp > 70){
+    goToTheBeach();
+}
+if(temp == 75){
+    goToTheBeach();
+} // two equal signs a for comparing the values
