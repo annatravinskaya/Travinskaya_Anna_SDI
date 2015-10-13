449a3450ebacf66a55eae901f8e023e7500f6a63
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-13 16:15:54 +0200

    Conditional logic practice
    Conditional logic practice

---
+++ Conditionals/Conditionals Example/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by annatravinskaya on 13/10/15.
+ */
+
+//Conditional Logic
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+    //code performed if the condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("what comes after")
---
+++ Conditionals/Nesting Conditionals/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by annatravinskaya on 13/10/15.
+ */
+
+var warmWater;
+var wearNewSuit;
+
+if(sunny) {
+    goToTheBeach(); //make an indent after to show which statement is inside the other
+
+    if (warmWater) {
+        wearNewSuit(); //putting one condition into the other to make sure right actions happen at the right place
+    }
+}else{
+    goToTheMovies();
