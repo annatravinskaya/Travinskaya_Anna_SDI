73031b61aff44553d122c122a5fe644ae19cf43d
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-13 17:18:52 +0200

    If and else practice.
    If and else practice. Code didn’t work.

---
+++ Conditionals/If and else/js/script.js
@@ -0,0 +1,13 @@
+//Conditional Logic - with an expression
+
+var kidHeight = 52;
+var minHeight = 48;
+
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}
