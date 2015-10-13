46bbd85df18df903615892b5758b143e42b9ca3c
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-13 20:27:13 +0200

    Practiced the or operator.
    Practiced the or operator.

--- Conditionals/Logical Operators/js/script.js
+++ Conditionals/Logical Operators/js/script.js
@@ -1,11 +1,11 @@
 //Conditional Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
