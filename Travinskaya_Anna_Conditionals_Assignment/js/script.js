97c389466f00b639e14b6a2ba9cfc784d1667de5
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-16 05:28:52 +0200

    Created some outputs and validations.
    Created some outputs and validations for prompts.

--- Travinskaya_Anna_Conditionals_Assignment/js/script.js
+++ Travinskaya_Anna_Conditionals_Assignment/js/script.js
@@ -14,3 +14,34 @@ var discountPercentage           = Number(prompt ("Enter discount percentage for
 //Calculations to find out the cost of the computer with the discount
 var discountedAmount             = (originalPriceOfTheComputer * discountPercentage)/100;
 var newComputerPrice             = originalPriceOfTheComputer - discountedAmount;
+
+
+//Outputs
+console.log("You would like to buy an " + nameOfTheComputer + ".");
+
+//Validation whether the user has entered the name
+if(nameOfTheComputer = ""){
+    prompt("Please, enter the name of the computer you would like to buy: ");
+}
+
+
+//Output for computer specifications
+console.log("It has a " + computerScreenSize + " inch display and " + computerMemory + " GB memory.");
+
+//Validation whether the user has entered the specifications
+if (computerScreenSize = ""){
+    prompt("Please, enter computer's screen size in inches: ");
+}
+
+if(computerMemory = ""){
+    prompt("Please, enter computer's screen size in inches: ");
+}
+
+//Validation if the user has entered information about the price
+if(originalPriceOfTheComputer = ""){
+    prompt("Please, enter the original price of the computer: ");
+}
+if(discountPercentage = ""){
+    prompt("Please, enter discount percentage for the computer: ");
+}
+
