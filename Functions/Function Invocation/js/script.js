888d69ea505c4269b75e920e9b2cb24820dc6faf
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-21 12:09:09 +0200

    Performed function invocation
    Performed function invocation.

---
+++ Functions/Function Invocation/js/script.js
@@ -0,0 +1,18 @@
+//Functions - Function Invocation
+
+function outputMsg(){
+    console.log("Hello World");
+}
+
+//function definition
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+//function invocation or execution
+calcArea();
+calcArea();
+calcArea();
