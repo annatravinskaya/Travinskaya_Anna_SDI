149ae7287e8df82f56bf88fdba23442e6f34c0ed
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-22 18:56:02 +0200

    Practiced loops
    Practiced loops

---
+++ Loops/js/script.js
@@ -0,0 +1,28 @@
+
+console.log("----Loops-----");
+
+var b = 10; //setups the index
+
+while ( b > 0 ){ //check the condition
+    console.log(b + " kegs on the wall");
+    b--; //increments or decrements the index
+}
+
+
+
+console.log("-----Do While Loops------");
+var c = 10;
+
+do{
+    console.log( c + " kegs on the wall");
+    c--;
+}
+while(c > 0);
+
+
+
+console.log ("-------For Loops------");
+
+for ( var i = 10; i > 0; i--){
+    console.log(i + " kegs on the wall")
+}
