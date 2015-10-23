a1ed22b7f567339c99449e60c4e61328cf284a50
annatravinskaya <atravinskaya@fullsail.edu>
2015-10-23 05:32:59 +0200

    validated user prompts with while loop
    validated user prompts with while loop

--- Travinskaya_Anna_Functions_Assignment/js/script.js
+++ Travinskaya_Anna_Functions_Assignment/js/script.js
@@ -9,3 +9,12 @@ var floridaNumbers = [];
 var powerballNumbers = [];
 var powerBall;
 
+//validated user prompts with while loop
+while(lotteryType === "Florida"){
+    console.log("You are playing Florida lottery!");
+    lotteryType++;
+}
+
+while(lotteryType === "Powerball"){
+    console.log("You are playing Powerball lottery!")
+    lotteryType++;
